d86d42e914f2c64c2406f75db2b448b32262b4eb
thor2643 <Thor951753@hotmail.com>
2024-07-23 10:16:22 +0200

    fix updateCover

--- Code/TCPServer/GUI_Server/src/GUIServerSmall.cpp
+++ Code/TCPServer/GUI_Server/src/GUIServerSmall.cpp
@@ -82,8 +82,8 @@ class UR5GUISmall : public App {
                     _DispenserClient->Send(send_msg);
                     
                     //Update components
-                    bot_cover_num = atoi(reinterpret_cast<const char*>(send_msg));
-                    bot_cover_num = atoi(reinterpret_cast<const char*>(send_msg));
+                    bot_cover_num = atoi(reinterpret_cast<const char*>(send_msg[0]));
+                    top_cover_num = atoi(reinterpret_cast<const char*>(send_msg[1]));
                     //bot_cover_num = atoi((const char*)send_msg[0]);
                     //top_cover_num = atoi((const char*)send_msg[1]);
 
